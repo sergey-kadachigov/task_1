6fe113062856b2475c8cd300c5121280c1682edc
sergey.kadachigov <sergey.kadachigov@PC1528.W3>
2017-09-26 16:51:10 +0300

    Initial commit

---
+++ markup/js/jquery.main.js
@@ -0,0 +1,101 @@
+jQuery(function () {
+	initOpenPanel();
+});
+
+function initOpenPanel() {
+	jQuery('.fixed-panel').openPanel({});
+};
+
+//initOpenPanel
+;(function ($) {
+	function OpenPanel(options) {
+		this.options = $.extend({
+			x: 'left',
+			y: 'top',
+			position: 'left',
+			holder: '.fixed-panel',
+			btn: '.panel-btn',
+			btnClose: 'btn-close',
+			activeClass: 'open',
+			inactiveClass: 'close',
+			btnActiveClass: 'active'
+
+		}, options);
+		this.init();
+	}
+
+	OpenPanel.prototype = {
+		init: function () {
+			if (this.options.holder) {
+				this.findElements();
+				this.attachEvents();
+				this.positionPanel();
+			}
+		},
+		findElements: function () {
+			this.holder = $(this.options.holder);
+			this.btn = $(this.options.btn);
+			this.positionOpt = eval('(' + this.holder.attr('data-options') + ')') || {
+				x: this.options.x,
+				y: this.options.y,
+				position: this.options.position
+			};
+			this.x = this.positionOpt.x;
+			this.y = this.positionOpt.y;
+			this.position = this.positionOpt.position;
+
+		},
+		attachEvents: function () {
+			var self = this;
+			this.btn.on('click', function (e) {
+				e.preventDefault();
+				self.panelAction($(this));
+			})
+		},
+		positionPanel: function () {
+			var self = this;
+			self.holder.addClass(self.y + ' ' + self.x + ' ' + 'animate-' + self.position)
+
+		},
+		panelAction: function (item) {
+			var self = this;
+			item.toggleClass(self.options.btnActiveClass);
+			if (item.hasClass(self.options.btnActiveClass)) {
+				self.panelOpen();
+			} else {
+				self.panelClose();
+			}
+		},
+		panelOpen: function () {
+			var self = this;
+			self.holder.addClass(self.options.activeClass);
+			self.holder.removeClass(self.options.inactiveClass);
+
+
+		},
+		panelClose: function () {
+			var self = this;
+			self.holder.removeClass(self.options.activeClass);
+			self.holder.addClass(self.options.inactiveClass);
+		}
+	};
+
+	// jquery plugin
+	$.fn.openPanel = function (opt) {
+		return this.each(function () {
+			$(this).data('OpenPanel', new OpenPanel($.extend({
+				holder: this
+			}, opt)));
+		});
+	};
+}(jQuery));
+
+
+// body.on('click', function (e) {
+// 	if (self.holder.has(e.target).length === 0) {
+// 		self.panelClose();
+// 		self.btn.removeClass(self.options.btnActiveClass);
+// 	} else {
+//
+// 	}
+// });
